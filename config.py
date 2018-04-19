5b9ca5df48cd3e160891126cabb9de795018b58c
sami-mai <samirah.maison@gmail>
2018-04-19 15:03:21 +0300

    update class config

--- config.py
+++ config.py
@@ -1,6 +1,20 @@
+import os
+
+
 class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 
 class ProdConfig(Config):
