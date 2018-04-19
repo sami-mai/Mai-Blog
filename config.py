f8812ebc3840dff9cb2536357c66e788185557dd
sami-mai <samirah.maison@gmail>
2018-04-19 14:11:16 +0300

    initial commit

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
 class Config:
-    pass
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+
 
 class ProdConfig(Config):
     pass
 
+
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sami_mai:SmaiDB@localhost/maiblog'
     DEBUG = True
 
-config_options ={"production":ProdConfig,"default":DevConfig}
 
+config_options = {
+    "production": ProdConfig,
+    "default": DevConfig
+    }
--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 
-from flask_script import Manager,Server
-from app import create_app,db
+from flask_script import Manager, Server, Shell
+from app import create_app, db
+# from app.models import User
 
 app = create_app('default')
 
@@ -8,6 +9,11 @@ manager = Manager(app)
 
 manager.add_command('server', Server)
 
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db)  #, User=User)
+
+
 if __name__ == '__main__':
     manager.run()
-
