8c6d144ad943b75e247a6ad3ea51e6ff089ccf3a
sami-mai <samirah.maison@gmail>
2018-04-20 12:32:04 +0300

    add blog post class

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class User(db.Model):
     password = password_hash = db.Column(db.String(255))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     comment = db.relationship('Comment', backref='user', lazy="dynamic")
-    post = db.relationship('Post', backref='user_admin', lazy="dynamic")
+    blogPost = db.relationship('BlogPost', backref='user_admin', lazy="dynamic")
 
     @property
     def password(self):
@@ -37,7 +37,6 @@ class Role(db.Model):
     name = db.Column(db.String(255))
     users = db.relationship('User', backref='role', lazy="dynamic")
 
-
     def __repr__(self):
         return f'User {self.name}'
 
@@ -48,6 +47,34 @@ class Subscriber(db.Model):
     name = db.Column(db.String(255))
     email = db.Column(db.String(255), unique=True, index=True)
 
-
     def __repr__(self):
         return f'User {self.name}'
+
+
+class BlogPost(db.Model):
+    '''
+    BlogPost class to define blog post Objects
+    '''
+
+    __tablename__ = 'blogPosts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    blogPost_category = db.Column(db.String)
+    blogPost_title = db.Column(db.String)
+    blogPost_description = db.Column(db.String)
+    blogPost_body = db.Column(db.String)
+    posted_on = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    def save_blogPost(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def clear_blogPosts(cls):
+        BlogPost.all_blogPosts.clear()
+
+    @classmethod
+    def get_blogPosts(cls, id):
+        blogPosts = BlogPost.query.filter_by(blogPost_id=id).all()
+        return blogPosts
