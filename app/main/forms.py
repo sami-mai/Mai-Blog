108acd94fb753dfc121fda8f17944cda84ff7065
sami-mai <samirah.maison@gmail>
2018-04-20 11:03:07 +0300

    add post, profile and subscription forms

---
+++ app/main/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import Required
+
+
+class BlogPostForm(FlaskForm):
+
+    category = StringField('Article category', validators=[Required()])
+    title = StringField('Article title', validators=[Required()])
+    description = StringField('Description title', validators=[Required()])
+    article = TextAreaField('Type your article here', validators=[Required()])
+    submit = SubmitField('Add Article')
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.', validators=[Required()])
+    submit = SubmitField('Update')
+
+
+class SubcriptionForm(FlaskForm):
+
+    name = StringField('Pitch genre', validators=[Required()])
+    title = StringField('Pitch title', validators=[Required()])
+    pitch = TextAreaField('Type your pitch here', validators=[Required()])
+    submit = SubmitField('Subscribe')
