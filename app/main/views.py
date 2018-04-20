9cf672453c83f1bde73db0483d8fe46f8005f939
sami-mai <samirah.maison@gmail>
2018-04-20 11:01:36 +0300

    add route for landing page

--- app/main/views.py
+++ app/main/views.py
@@ -4,4 +4,5 @@ from flask import render_template
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    title = "Mai Blog"
+    return render_template('index.html', title=title)
