7d7737dfc143af24e9a1d63e685ef33a2bc90664
sami-mai <samirah.maison@gmail>
2018-04-21 20:14:59 +0300

    import datetime module

--- app/models.py
+++ app/models.py
@@ -1,4 +1,9 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+# from flask_login import UserMixin
+# from . import login_manager
+
 
 
 class User(db.Model):
