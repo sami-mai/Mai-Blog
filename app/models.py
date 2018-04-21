ee22bb14857a8caa2c3ce47eec7865ae14df7fa6
sami-mai <samirah.maison@gmail>
2018-04-21 21:25:26 +0300

    add login configurations

--- app/models.py
+++ app/models.py
@@ -1,18 +1,22 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-# from flask_login import UserMixin
-# from . import login_manager
+from flask_login import UserMixin
+from . import login_manager
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     fname = db.Column(db.String(255))
     email = db.Column(db.String(255), unique=True, index=True)
     uname = db.Column(db.String(255))
-    password = password_hash = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     comment = db.relationship('Comment', backref='user', lazy="dynamic")
     blogPost = db.relationship('BlogPost', backref='user_admin', lazy="dynamic")
