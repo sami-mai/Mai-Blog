35db8dbda410e84277d4eb21d312f6295928415b
sami-mai <samirah.maison@gmail>
2018-04-21 20:31:43 +0300

    register the auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -13,11 +13,14 @@ def create_app(config_state):
     app = Flask(__name__)
     app.config.from_object(config_options[config_state])
 
-
     bootstrap.init_app(app)
     db.init_app(app)
 
+    # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
 
     return app
