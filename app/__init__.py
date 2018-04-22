9049177909c295b98e7bad94034b230ed857b78d
sami-mai <samirah.maison@gmail>
2018-04-22 20:46:26 +0300

    import flask mail

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,33 @@
-
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
 # Create instance of flask extensions
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
+photos = UploadSet('photos', IMAGES)
+mail = Mail()
+simple = SimpleMDE()
 
 
 def create_app(config_state):
     app = Flask(__name__)
     app.config.from_object(config_options[config_state])
 
+    # configure UploadSet
+    configure_uploads(app, photos)
+
 # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    mail.init_app(app)
+    simple.init_app(app)
     login_manager.init_app(app)
     login_manager.session_protection = 'strong'
     login_manager.login_view = 'auth.login'
