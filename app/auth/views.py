67a31e6e2b7bce906a4d1101fbc315e9789dfc8e
sami-mai <samirah.maison@gmail>
2018-04-22 09:51:43 +0300

    add view routes

--- app/auth/views.py
+++ app/auth/views.py
@@ -7,6 +7,21 @@ from .. import db
 from ..email import mail_message
 
 
+@auth.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to Mai Blog", "email/welcome_user", user.email, user=user)
+
+        return redirect(url_for('auth.login'))
+    title = "Mai Blog"
+    return render_template('auth/register.html', registration_form=form, title=title)
+
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
@@ -27,18 +42,3 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("main.index"))
-
-
-@auth.route('/register', methods=["GET", "POST"])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
-        db.session.add(user)
-        db.session.commit()
-
-        mail_message("Welcome to Mai Blog", "email/welcome_user", user.email, user=user)
-
-        return redirect(url_for('auth.login'))
-    title = "Mai Blog"
-    return render_template('auth/register.html', registration_form=form, title=title)
